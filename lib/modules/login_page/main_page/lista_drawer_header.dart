859d77adbcd8881a4703d329e59c94e553e9eafa
Daniel Briquez <dbriquez.devmaua@gmail.com>
2022-08-01 18:27:25 -0300

    app bar interativa pt.1

--- lib/modules/login_page/main_page/lista_drawer_header.dart
+++ lib/modules/login_page/main_page/lista_drawer_header.dart
@@ -1,9 +1,4 @@
-import 'dart:js';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MyDrawerList extends StatefulWidget {
   const MyDrawerList({Key? key}) : super(key: key);
@@ -13,60 +8,63 @@ class MyDrawerList extends StatefulWidget {
 }
 
 class _MyDrawerListState extends State<MyDrawerList> {
-  @override
+  var currentPage = DrawerSections.meu_perfil;
   Widget build(BuildContext context) {
-    var currentPage;
     return Container(
-      padding: EdgeInsets.only(top: 15),
+      padding: const EdgeInsets.only(top: 15),
       child: Column(
         children: [
           menuItem(1, "Meu Perfil", Icons.account_circle_outlined,
               currentPage == DrawerSections.meu_perfil ? true : false),
           menuItem(2, "Feed Principal", Icons.add_home_outlined,
               currentPage == DrawerSections.main_feed ? true : false),
-          Divider(),
+          const Divider(),
           menuItem(3, "Configurações", Icons.settings,
               currentPage == DrawerSections.settings ? true : false),
           menuItem(4, "Reportar Erros", Icons.notifications_outlined,
               currentPage == DrawerSections.login_page ? true : false),
           menuItem(5, "Sair", Icons.assignment_return_outlined,
               currentPage == DrawerSections.error_report ? true : false),
-          Divider(),
+          const Divider(),
         ],
       ),
     );
   }
-}
 
-Widget menuItem(int id, String title, IconData icon, bool selected) {
-  var currentPage;
-  return Material(
-      color: selected ? Colors.grey[300] : Colors.transparent,
-      child: InkWell(
-        onTap: () {
-          Navigator.pop(context);
-          setState(() {
-            if (id == 1) {
-              currentPage = DrawerSections.meu_perfil;
-            } else if (id == 2) {
-              currentPage = DrawerSections.main_feed;
-            }
-            //continuar aqui
-          });
-        },
-        child: Padding(
-          padding: EdgeInsets.all(15),
-          child: Row(children: [
-            Expanded(child: Icon(icon, size: 20, color: Colors.black)),
-            Expanded(
-                flex: 3,
-                child: Text(
-                  title,
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                )),
-          ]),
-        ),
-      ));
+  Widget menuItem(int id, String title, IconData icon, bool selected) {
+    return Material(
+        color: selected ? Colors.grey[300] : Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+            setState(() {
+              if (id == 1) {
+                currentPage = DrawerSections.meu_perfil;
+              } else if (id == 2) {
+                currentPage = DrawerSections.main_feed;
+              } else if (id == 3) {
+                currentPage = DrawerSections.settings;
+              } else if (id == 4) {
+                currentPage = DrawerSections.error_report;
+              } else if (id == 5) {
+                currentPage = DrawerSections.login_page;
+              }
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(children: [
+              Expanded(child: Icon(icon, size: 20, color: Colors.black)),
+              Expanded(
+                  flex: 3,
+                  child: Text(
+                    title,
+                    style: const TextStyle(color: Colors.black, fontSize: 16),
+                  )),
+            ]),
+          ),
+        ));
+  }
 }
 
 enum DrawerSections {
